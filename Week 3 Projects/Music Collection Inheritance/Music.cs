f1a7621cbb278ae8af8af8c25152c6685347ff76
Cassie Christensen <casandralekai1203@gmail.com>
2022-04-20 10:24:32 -0500

    Music Collection, Base Music Done.

--- Week 3 Projects/Music Collection Inheritance/Music.cs
+++ Week 3 Projects/Music Collection Inheritance/Music.cs
@@ -2,41 +2,58 @@ using System;
 
 namespace MyApplication
 {
-  class Music //This is my base class (Parent)
-  { 
+     class Music //This is my base class (Parent)
+    { 
     
-    //Create Class Members. AKA fields and methods.
+        //Create Class Members. AKA fields and methods.
 
-    //These are my fields. AKA instance variables that are declared inside of a class. I'll be able to access these when I create an object of the class.
-    //They are private so only methods of the object can access it. 
+     //These are my fields. AKA instance variables that are declared inside of a class. I'll be able to access these when I create an object of the class.
+     //They are private so only methods of the object can access it. 
 
-      private string artistName;
-      private string songTitle;
+      private string artistName; //my field
+      public string getName()  //property longhand
+      {
+          return artistName;
+      }
 
+      public void setName (string ArtistName)
+      {
+          artistName = ArtistName;
+      }
 
-    //This is my default constructor. AKA it is a special method. It is used when no values are being passed.
-    public Music () //Constructors will have the same name as the class
-    {
+      
+      public string SongTitle    // property shorthand
+      { get; set; }  
+
+      //This is my default constructor. AKA it is a special method. It is used when no values are being passed.
+     public Music () //Constructors will have the same name as the class
+      {
         artistName = null;
-        songTitle = null;
-    }
+        SongTitle = null;
+      }
 
-    //This is the constructor when two values are being passed. This constructor allows me to use the private variables.
-    public Music (string aArtistName, string aSongTitle)
-    {
+      
+
+       //This is the constructor when two values are being passed. This constructor allows me to use the private variables.
+      /* public Music (string aArtistName, string aSongTitle)
+      {
         artistName = aArtistName;
         songTitle = aSongTitle;
-    }
+     } */
 
-    //This overrides ToString so an object can be printed out with the WriteLine.
-    public override string ToString()
-    {
-        return "The artist's name is: " + artistName + " and their song title is called: " + songTitle;
-    }
-    
-    public void GoatMethod()
-    {
-    Console.WriteLine(artistName + " is the G.O.A.T.");
-    }
-  }// class Muisc
-}// namespace MyApplication
+
+
+     //This overrides ToString so an object can be printed out with the WriteLine.
+      public override string ToString()
+       {
+        return "The artist's name is: " + artistName + " and their song title is called: " + SongTitle;
+      }
+
+      
+        //a fun method
+      public void GoatMethod()
+      {
+      Console.WriteLine(artistName + " is the G.O.A.T.");
+      }
+    }// class Music
+} // namespace
--- Week 3 Projects/Music Collection Inheritance/Program.cs
+++ Week 3 Projects/Music Collection Inheritance/Program.cs
@@ -6,13 +6,24 @@ namespace MyApplication
   { 
     static void Main(string[] args)
     {
-      //Create a single object to test that the class Music is working 
-      Music myMusic = new Music("Taylor Swift", "Peace");
-      Music myMusic2 = new Music("Eminem", "Stan"); 
+      //Create a single object to test that the class Music is working. Use a constructor to pass the variables.
+      /* Music myMusic = new Music("Taylor Swift", "Peace");
+      Music myMusic2 = new Music("Eminem", "Stan");  */
+
+      //Create single objects to test that the class Music is working. Use get/set method though. Both short and long hand. 
+      Music myMusic = new Music();
+      myMusic.SongTitle = "Goodnight My Angel";
+      myMusic.setName("Billy Joel");
+
+      Music myMusic2 = new Music();
+      myMusic2.setName("Eminem");
+      myMusic2.SongTitle = "Stan";
 
       //Call the ToString Console.WriteLine
       Console.WriteLine(myMusic);
       Console.WriteLine(myMusic2);
+
+      //Call method
       myMusic2.GoatMethod();
 
     }
