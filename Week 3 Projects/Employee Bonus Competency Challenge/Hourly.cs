8a347222041b6ef5be07f4404b4b41f0773afb75
Cassie Christensen <casandralekai1203@gmail.com>
2022-04-21 11:41:44 -0500

    Hourly Class is working

---
+++ Week 3 Projects/Employee Bonus Competency Challenge/Hourly.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace MyApplication
+{
+  class Hourly : Employee // derived(child) : base(parent)
+  {
+      
+      //Create my private fields and my properties using short hand. Because I am using shorthand, I do not need to find the field of the property.
+      public float HourlyPay
+      { get; set; }
+ 
+
+      //Create my default constructor
+      public Hourly ()
+      {
+          HourlyPay = 0.0f;
+      }
+      
+      //Create my constructor that passes values
+      public Hourly (float aHourlyPay)
+      {
+          HourlyPay = aHourlyPay;
+      }
+      
+      //Calculate the bonus 
+        public int HourlyBonusMethod ()
+        {
+            double doubleHourlyPay = (double) HourlyPay;
+            double doubleHourlyBonus = doubleHourlyPay * 80;
+            int intHourlyBonus = (int) doubleHourlyBonus;
+
+            return intHourlyBonus;
+        }
+
+      //Create my polymorphism. Use ToString Method.
+      public override string ToString ()
+      {
+          return FirstName + " " + LastName + " is an " + EmployeeType + " employee. Their bonus will be: " + HourlyBonusMethod() + ".";
+      }
+
+    
+  } //Employee class
+} //Namespace
--- Week 3 Projects/Employee Bonus Competency Challenge/Program.cs
+++ Week 3 Projects/Employee Bonus Competency Challenge/Program.cs
@@ -11,12 +11,21 @@ namespace MyApplication
         Employee testEmployee = new Employee();
         testEmployee.LastName = "Christensen";
         testEmployee.FirstName = "Austin";
-        testEmployee.EmployeeType = 'H';
+        testEmployee.EmployeeType = 'E';
+
+        //Create a test object for Hourly 
+        Hourly testHourly = new Hourly ();
+        testHourly.LastName = "Weisser";
+        testHourly.FirstName = "Lauren";
+        testHourly.EmployeeType = 'H';
+        testHourly.HourlyPay = 18.96F;
         
 
         //Test the toString 
         Console.WriteLine(testEmployee);
 
+        //Test the toString 
+        Console.WriteLine(testHourly);
 
     }
   }
