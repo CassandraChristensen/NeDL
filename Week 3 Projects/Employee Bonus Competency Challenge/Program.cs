c773c749390156b7837a078a43e3134721de51f3
Cassie Christensen <casandralekai1203@gmail.com>
2022-04-21 11:48:59 -0500

    Salary class is working

--- Week 3 Projects/Employee Bonus Competency Challenge/Program.cs
+++ Week 3 Projects/Employee Bonus Competency Challenge/Program.cs
@@ -8,7 +8,7 @@ namespace MyApplication
     {
       
         //Create a test object for Employee 
-        Employee testEmployee = new Employee();
+        Employee testEmployee = new Employee();   ///This is creating my object, and calling the default constructor to give default values to the fields.
         testEmployee.LastName = "Christensen";
         testEmployee.FirstName = "Austin";
         testEmployee.EmployeeType = 'E';
@@ -19,6 +19,13 @@ namespace MyApplication
         testHourly.FirstName = "Lauren";
         testHourly.EmployeeType = 'H';
         testHourly.HourlyPay = 18.96F;
+
+        //Create a test object for Salary
+        Salary testSalary = new Salary (); 
+        testSalary.LastName = "Crandall";
+        testSalary.FirstName = "Stephanie";
+        testSalary.EmployeeType = 'S';
+        testSalary.SalaryPay = 65000;
         
 
         //Test the toString 
@@ -26,6 +33,9 @@ namespace MyApplication
 
         //Test the toString 
         Console.WriteLine(testHourly);
+
+        //Test the toString
+        Console.Write(testSalary);
 
     }
   }
