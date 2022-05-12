6958a32890e524f7ba371b6e279f59db15323ad9
Cassie Christensen <casandralekai1203@gmail.com>
2022-05-12 13:19:54 -0500

    Clear all function works

--- Week 6 Projects/Competency Project/toDo.ts
+++ Week 6 Projects/Competency Project/toDo.ts
@@ -15,11 +15,43 @@ function AddFunction(){
     else {
         this.tableRef = document.getElementById("myTable");
         (this.tableRef.insertRow(this.tableRef.rows.length)).innerHTML = addItem;
+        document.forms["myForm"]["itemName"].value = "";
         return true;
-
     }
 
-}
+}//end add function 
+
 //2. Create Delete Function
+function DeleteFunction(){
+    //a. Declare variable, label the type, go to document>forms>[form name][formitem], insert that value into variable
+    var deleteItem : string = document.forms["myForm"]["itemName"].value;
+
+    //b. Validate the value entered, if  not return with an error 
+    if (deleteItem == "") {
+        alert("Please enter an item to delete from the list.");
+        return false;
+    }
+    //c. Create the table variable, assign the value to it by using, GetElementID, then do a for loop to search through the table and find the word. 
+    else {
+        this.tableRef = document.getElementById("myTable");
+
+        /* for(let index : number = 0, row; row = this.tableRef.rows[index]; index++) {
+            //loop through rows 
+            if (this.tableRef.row[index].innerHTML == deleteItem){
+                this.tableRef.row[index] == "";
+            }
+        } */
+       
+
+
+
+    }
+}//end delete function 
 //3. Create Complete Function (Optional)
-//4. Create Clear All Function 
+
+//4. Create Clear All Function: Done 
+
+function ClearAllFunction(){
+    this.tableRef = document.getElementById("myTable");
+    this.tableRef.innerHTML = "";
+}
