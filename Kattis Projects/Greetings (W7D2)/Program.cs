a4b2e21c4fe5ee01d0e5b47c7da601b32508038f
Cassie Christensen <casandralekai1203@gmail.com>
2022-05-17 14:38:11 -0500

    works

---
+++ Kattis Projects/Greetings (W7D2)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      //Prompt user to enter greeting
+      Console.WriteLine("Please enter your greeting.");
+      string greeting = Console.ReadLine()!;
+
+     //
+      string section = greeting.Substring(1, greeting.Length - 2);
+
+      Console.WriteLine(section);
+
+      string result = "";
+
+      for (int index = 0; index<section.Length*2; index++)
+      {
+          result += "e";
+      }
+
+      Console.WriteLine("h" + result + "y");
+    } //main
+  } //class program
+} //namespace
+
+
+
+
