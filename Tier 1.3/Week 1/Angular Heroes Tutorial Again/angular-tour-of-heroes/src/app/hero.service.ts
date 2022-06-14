1faba2078d1e03cdc47ef24d7131aa4779eac13c
Cassie Christensen <casandralekai1203@gmail.com>
2022-06-14 17:50:06 -0500

    Broke it trying to make HTTP work

--- Tier 1.3/Week 1/Angular Heroes Tutorial Again/angular-tour-of-heroes/src/app/hero.service.ts
+++ Tier 1.3/Week 1/Angular Heroes Tutorial Again/angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,7 +3,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 //Injectable decorator marks this class as one that participates in Dependency Injection. 
@@ -13,14 +13,25 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';  // URL to web api
+
   //This is a typical "service-in-service" scenario: You inject the MessageService into the HeroService which is injected into the HeroesComponent.
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
   
 }
