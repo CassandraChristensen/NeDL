1bda5bc97eaf586f6e0fe9ded0732a14593df555
Cassie Christensen <casandralekai1203@gmail.com>
2022-06-21 15:54:51 -0500

    Backend works

--- Tier 1.3/Week 2/Day 1/Activity4Again/Backend/Backend/Program.cs
+++ Tier 1.3/Week 2/Day 1/Activity4Again/Backend/Backend/Program.cs
@@ -1,3 +1,6 @@
+using Backend;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure the database context service.
+builder.Services.AddDbContext<ShoppingDatabase>(options =>
+    options.UseSqlServer("Server=(local); Integrated Security=true; Database=Shopping"));
 
 builder.Services.AddCors();
 
---
+++ Tier 1.3/Week 2/Day 1/Activity4Again/Backend/Backend/ShoppingDatabase.cs
@@ -0,0 +1,15 @@
+﻿namespace Backend
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ShoppingDatabase : DbContext
+    {
+
+        public ShoppingDatabase(DbContextOptions<ShoppingDatabase> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<ShoppingListItem> ShoppingListItems { get; set; }
+    }
+}
